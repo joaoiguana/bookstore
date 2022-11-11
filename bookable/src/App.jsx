ea3cc58b1ab2333bcba207130a3fa84ff684d0b1
joao garcia <jiboiadamor@gmail.com>
2022-11-11 15:51:44 +0000

    fully functional web app with rails api and react front

--- bookable/src/App.jsx
+++ bookable/src/App.jsx
@@ -1,9 +1,31 @@
 import './App.css';
+import axios from "axios";
+import Books from './components/Books';
+import { useEffect, useState } from "react";
+
+const API_URL = "http://localhost:3000/api/v1/books";
+
+const getAPIData = () => {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then((items) => {
+      if (mounted) {
+        setBooks(items);
+      }
+    })
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Books books={books} />
     </div>
   );
 }
